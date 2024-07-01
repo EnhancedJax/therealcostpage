55a7d1af08317d4303dd6c8b4b88198fbb68cab8
Jax <enhancedjax@gmail.com>
2024-07-01 13:42:09 +0800

    PrivacyPolicy

---
+++ src/app/privacypolicy/page.js
@@ -0,0 +1,20 @@
+export default function PrivacyPolicy() {
+  return (
+    <main className="flex flex-col min-h-screen">
+      <h1 className="text-2xl font-bold">Privacy Policy - The Real Cost</h1>
+      <h2>What information does the extension obtain and how is it used?</h2>
+      <p>
+        No user data is collected. The Application does not send any information
+        remotely when you download and use it. Only information you provide will
+        be stored locally. Registration is not required to use the Application.
+      </p>
+
+      <h2>Contact Us</h2>
+      <p>
+        If you have any questions regarding privacy while using the Application,
+        or have questions about the practices, please contact the Service
+        Provider via email at enhancedjax@gmail.com.
+      </p>
+    </main>
+  );
+}
