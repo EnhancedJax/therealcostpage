49d233dc4129d91ca36f50bcb420c0e246fad674
Jax <enhancedjax@gmail.com>
2024-07-24 14:10:49 +0800

    SEO

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,9 +4,9 @@ import "./globals.css";
 const family = Kumbh_Sans({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "The Real Cost",
+  title: "The Real Cost - Convert prices into time",
   description:
-    "A Chrome extension that converts prices on websites to hours of work needed to afford them.",
+    "A Chrome extension that converts prices on websites to hours of work needed to afford them. Install the extension today!",
 };
 
 export default function RootLayout({ children }) {
