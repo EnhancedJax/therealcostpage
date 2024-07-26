365ec42578e0099b277ece304966133bff9e1971
Jax <enhancedjax@gmail.com>
2024-07-26 15:02:12 +0800

    Featured badge

--- src/app/mobile/page.js
+++ src/app/mobile/page.js
@@ -1,3 +1,4 @@
+import Laurel from "@/components/icons/Laurel";
 import StoreButton from "@/components/StoreButton";
 import DeviceRoute from "@/lib/deviceRoute";
 import Image from "next/image";
@@ -13,11 +14,22 @@ export default async function Mobile() {
         <Image src={Logo} alt="Logo" width={100} height={100} />
 
         <h1 className="w-full pt-4 text-lg text-center">The Real Cost</h1>
+
         <p>
           A Chrome extension that converts prices on websites to hours of work
           needed to afford them.
         </p>
 
+        <div className="flex justify-center mt-6 mb-2 h-11">
+          <div className="flex h-full gap-2">
+            <Laurel />
+            <h2 className="flex flex-col font-normal text-center">
+              <span className="text-xs">Chrome Web Store</span>
+              <span className="">Featured Extension</span>
+            </h2>
+            <Laurel className="transform scale-x-[-1]" />
+          </div>
+        </div>
         <StoreButton>View on Chrome Web Store</StoreButton>
       </main>
     </DeviceRoute>
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Field from "@/components/Field";
+import Laurel from "@/components/icons/Laurel";
 import StoreButton from "@/components/StoreButton";
 import Try from "@/components/Try";
 import { fetchExchangeRates } from "@/lib/api";
@@ -18,6 +19,16 @@ export default async function Home() {
           <h1 className="w-full pt-4 text-lg text-center opacity-20">
             TheRealCost
           </h1>
+          <div className="flex justify-center mt-4 h-11">
+            <div className="flex h-full gap-2">
+              <Laurel />
+              <h2 className="flex flex-col font-normal text-center">
+                <span className="text-xs">Chrome Web Store</span>
+                <span className="">Featured Extension</span>
+              </h2>
+              <Laurel className="transform scale-x-[-1]" />
+            </div>
+          </div>
           <Try rates={rates} />
           <StoreButton>Install extension</StoreButton>
           <Field rates={rates} />
